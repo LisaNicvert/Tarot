d65349796467997db0e292a35db5c82297ac05c4
Lisa Nicvert <lisa.n@orange.fr>
2022-12-29 17:44:59 +0100

    Added player count and names fields

--- app.R
+++ app.R
@@ -22,7 +22,27 @@ ui <- dashboardPage(
   dashboardBody(
     tabItems(
       tabItem(tabName = "quijoue",
-              h2("Qui joue ?")),
+              column(width = 12,
+                h2("Qui joue ?"),
+                numericInput("njoueurs", "Nombre de joueurs", 
+                             value = 3, min = 3, max = 5, step = 1,
+                             width = "150px")
+                ),
+              column(width = 2,
+                     textInput("J1", "Joueur-euse 1")),
+              column(width = 2,
+                     textInput("J2", "Joueur-euse 2")),
+              column(width = 2,
+                     textInput("J3", "Joueur-euse 3")),
+              column(width = 2,
+                conditionalPanel(condition = "input.njoueurs >= 4",
+                                 textInput("J4", "Joueur-euse 4"))
+                ),
+              column(width = 2,
+                conditionalPanel(condition = "input.njoueurs >= 5",
+                                 textInput("J5", "Joueur-euse 5"))
+                )
+              ),
       tabItem(tabName = "recap",
               fluidRow(
                 box(h2("Qui prend etc")),
